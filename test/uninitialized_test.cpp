f4b6f647b3c6f54b91f83b8e65819cbbbd1780ec
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2022-04-19 16:28:51 -0400

    Detab and modernize uninitialized_test.cpp

--- test/uninitialized_test.cpp
+++ test/uninitialized_test.cpp
@@ -11,61 +11,52 @@
 #include <vector>
 
 namespace {
-	struct lifetest {
-		static uint64_t construct;
-		static uint64_t destruct;
-		static uint64_t move;
-		lifetest()
-		{
-			++construct;
-		}
-		lifetest(lifetest&& /*in*/) noexcept
-		{
-			++move;
-		}
-		~lifetest()
-		{
-			++destruct;
-		}
-		static void reset()
-		{
-			construct = 0;
-			destruct = 0;
-			move = 0;
-		}
-
-		// To avoid "unused argument" error/warning. 
-		#ifdef NDEBUG
-			static void test(uint64_t, uint64_t, uint64_t)
-			{
-				
-			}
-		#else
-			static void test(uint64_t inconstruct, uint64_t indestruct, uint64_t inmove)
-			{
-				assert(construct == inconstruct);
-				assert(destruct == indestruct);
-				assert(move == inmove);
-			}
-		#endif
-		
-	};
-	uint64_t lifetest::construct;
-	uint64_t lifetest::destruct;
-	uint64_t lifetest::move;
+    struct lifetest {
+        static uint64_t construct;
+        static uint64_t destruct;
+        static uint64_t move;
+        explicit lifetest() {
+            ++construct;
+        }
+        lifetest(lifetest&&) noexcept {
+            ++move;
+        }
+        ~lifetest() {
+            ++destruct;
+        }
+        static void reset() {
+            construct = 0;
+            destruct = 0;
+            move = 0;
+        }
+        static void test(uint64_t inconstruct, uint64_t indestruct, uint64_t inmove) {
+            assert(construct == inconstruct);
+            assert(destruct == indestruct);
+            assert(move == inmove);
+        }
+    };
+    uint64_t lifetest::construct = 0;
+    uint64_t lifetest::destruct = 0;
+    uint64_t lifetest::move = 0;
 } // namespace
 
 TEST(uninitialized_value_construct, Basic)
 {
     for (int n = 0; n < 256; ++n) {
         auto m = (lifetest*)malloc(sizeof(lifetest) * n);
-        lifetest::test(0, 0, 0);
-        stdext::uninitialized_value_construct(m, m + n);
-        lifetest::test(n, 0, 0);
-        stdext::destruct(m, m + n);
-        lifetest::test(n, n, 0);
-        free(m);
         lifetest::reset();
+
+        stdext::uninitialized_value_construct(m, m + n);
+        ASSERT_EQ(lifetest::construct, n);
+        ASSERT_EQ(lifetest::destruct, 0);
+        ASSERT_EQ(lifetest::move, 0);
+
+        stdext::destruct(m, m + n);
+        ASSERT_EQ(lifetest::construct, n);
+        ASSERT_EQ(lifetest::destruct, n);
+        ASSERT_EQ(lifetest::move, 0);
+
+        free(m);
     }
 
     auto m = (int*)malloc(sizeof(int) * 5);
@@ -78,17 +69,25 @@ TEST(uninitialized_default_construct, Basic)
 {
     for (int n = 0; n < 256; ++n) {
         auto mem1 = (lifetest*)malloc(sizeof(lifetest) * n);
-        lifetest::test(0, 0, 0);
+        lifetest::reset();
+
         stdext::uninitialized_default_construct(mem1, mem1 + n);
-        lifetest::test(n, 0, 0);
+        ASSERT_EQ(lifetest::construct, n);
+        ASSERT_EQ(lifetest::destruct, 0);
+        ASSERT_EQ(lifetest::move, 0);
 
         auto mem2 = (lifetest*)malloc(sizeof(lifetest) * n);
         stdext::uninitialized_move(mem1, mem1 + n, mem2);
-        lifetest::test(n, 0, n);
+        ASSERT_EQ(lifetest::construct, n);
+        ASSERT_EQ(lifetest::destruct, 0);
+        ASSERT_EQ(lifetest::move, n);
+
         stdext::destruct(mem2, mem2 + n);
-        lifetest::test(n, n, n);
+        ASSERT_EQ(lifetest::construct, n);
+        ASSERT_EQ(lifetest::destruct, n);
+        ASSERT_EQ(lifetest::move, n);
+
         free(mem1);
         free(mem2);
-        lifetest::reset();
     }
 }
